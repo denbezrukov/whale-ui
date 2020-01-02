1191f008cfc2d991e674227605645703be13fbac
Dominionys <denis_bezrukov@mail.ru>
2020-01-02 22:15:44 +0200

    remove babel from jest

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,4 @@ module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
 };
